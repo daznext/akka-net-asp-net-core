6ee080e832a70603770f97dc43defd2788d8c2e7
daz <daz@mm.st>
2020-02-06 20:49:48 +0300

    3.0

--- src/Bookstore/Startup.cs
+++ src/Bookstore/Startup.cs
@@ -5,56 +5,47 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Bookstore
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Bookstore {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddRazorPages ();
 
             // Register ActorSystem
-            services.AddSingleton(_ => ActorSystem.Create("bookstore", ConfigurationLoader.Load()));
+            services.AddSingleton (_ => ActorSystem.Create ("bookstore", ConfigurationLoader.Load ()));
 
-            services.AddSingleton<BooksManagerActorProvider>(provider =>
-            {
-                var actorSystem = provider.GetService<ActorSystem>();
-                var booksManagerActor = actorSystem.ActorOf(Props.Create(() => new BooksManagerActor()));
+            services.AddSingleton<BooksManagerActorProvider> (provider => {
+                var actorSystem = provider.GetService<ActorSystem> ();
+                var booksManagerActor = actorSystem.ActorOf (Props.Create (() => new BooksManagerActor ()));
                 return () => booksManagerActor;
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env,IHostApplicationLifetime lifetime) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseHsts ();
             }
-            else
-            {
-                app.UseHsts();
-            }            
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseHttpsRedirection ();
+            app.UseRouting ();
+            app.UseEndpoints (endpints => { endpints.MapRazorPages (); });
 
-            lifetime.ApplicationStarted.Register(() =>
-            {
-                app.ApplicationServices.GetService<ActorSystem>(); // start Akka.NET
+            lifetime.ApplicationStarted.Register (() => {
+                app.ApplicationServices.GetService<ActorSystem> (); // start Akka.NET
             });
-            lifetime.ApplicationStopping.Register(() =>
-            {
-                app.ApplicationServices.GetService<ActorSystem>().Terminate().Wait();
+            lifetime.ApplicationStopping.Register (() => {
+                app.ApplicationServices.GetService<ActorSystem> ().Terminate ().Wait ();
             });
         }
     }
